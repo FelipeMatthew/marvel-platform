7d6f64c7f5636814be5b623d92213a9456346cdf
felipematthew <felipematthewnascimento@gmail.com>
2024-05-12 21:23:28 -0300

    :sparkles: Getting start frontend

---
+++ reactjs-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import { GlobalStyles } from "./styles/GlobalStyle";
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyles />
+      <h1>Hello world</h1>
+    </>
+  )
+}
+
+export default App
